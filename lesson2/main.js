1ecade341520c4c0886abbe5bdb6d97d38476521
aleksandrM <a.makarov@zinit.com>
2026-07-30 16:23:16 +0300

    lesson 2

--- lesson2/main.js
+++ lesson2/main.js
@@ -1,9 +1,39 @@
 window.onload = () => {
-    const button = document.querySelector('#but');
+    const div = document.querySelector('#button-container');
+    let previousEvent;
 
-    button.onclick = (event) => {
-        alert('nu ti i eblan');
+    const { width, height} = document.body.getBoundingClientRect();
 
-        console.log(event);
-    }
+
+    div.addEventListener('mousemove', (currentEvent) => {
+        if (previousEvent) {
+            const currentPosition = div.getBoundingClientRect();
+            let [diffX, diffY] = [
+                currentEvent.pageX - previousEvent.pageX + currentPosition.x,
+                currentEvent.pageY - previousEvent.pageY + currentPosition.y,
+            ];
+
+            if (
+                diffX < 0 ||
+                diffY < 0 ||
+                width < diffX + currentPosition.width ||
+                height < diffY + currentPosition.height
+            ) {
+                [diffX, diffY] = [
+                    width / 2 - currentPosition.width / 2,
+                    height / 2 - currentPosition.height / 2,
+                ];
+            }
+
+            div.style.left = `${diffX}px`;
+            div.style.top = `${diffY}px`;
+        }
+
+        previousEvent = currentEvent;
+    });
+
+    div.addEventListener('mouseleave', (event) => {
+        previousEvent = null;
+    })
 }
+
