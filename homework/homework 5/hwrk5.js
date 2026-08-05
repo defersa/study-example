8c51487a68a2f23869ffef2c5f194fa552a644f6
tipaproger <arturarturvuilov@gmail.com>
2026-08-05 13:35:29 +0300

    doneeeee

---
+++ homework/homework 5/hwrk5.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    const div = document.querySelector('#button-container');
+    const div2 = document.querySelector('#button-container2');
+    const { width, height } = document.body.getBoundingClientRect();
+    let previousEvent=0;
+
+    div2.addEventListener('click', (currentEvent) => {
+        const [horiz,vert] = [Math.floor(Math.random() * width)+1, Math.floor(Math.random() * height)+1];
+        previousEvent++;
+        div.innerHTML = previousEvent;
+        div2.style.left = `${horiz}px`;
+        div2.style.top = `${vert}px`;
+    })
+};
+
+
