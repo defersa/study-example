1e7d088ebf7b3004742282a3cd38801669ab943f
aleksandrM <a.makarov@zinit.com>
2026-08-07 11:54:09 +0300

    lesson 3

--- homework/homework 4/task1.js
+++ homework/homework 4/task1.js
@@ -1,24 +1,19 @@
 window.onload = () => {
     const divElement = document.querySelector('#button-container');
 
-    let previousEvent;
-
     const { width, height } = document.body.getBoundingClientRect();
 
     document.addEventListener('mousemove', (currentEvent) => {
 
-        if (previousEvent) {
+        const currentPosition = divElement.getBoundingClientRect();
 
-            const currentPosition = divElement.getBoundingClientRect();
+        let [diffX, diffY] = [
+            currentPosition.x + (currentEvent.pageX - previousEvent.pageX),
+            currentEvent.pageY - previousEvent.pageY + currentPosition.y
+        ];
 
-            let [diffX, diffY] = [
-                currentPosition.x + (currentEvent.pageX - previousEvent.pageX),
-                currentEvent.pageY - previousEvent.pageY + currentPosition.y
-            ];
-
-            divElement.style.left = `${diffX}px`;
-            divElement.style.top = `${diffY}px`;
-        }
+        divElement.style.left = `${diffX}px`;
+        divElement.style.top = `${diffY}px`;
 
         previousEvent = currentEvent;
     });
--- homework/homework 5/hwrk5.js
+++ homework/homework 5/hwrk5.js
@@ -1,16 +1,18 @@
 window.onload = () => {
     const div = document.querySelector('#button-container');
     const div2 = document.querySelector('#button-container2');
-    const { width, height } = document.body.getBoundingClientRect();
-    let previousEvent=0;
+    const {width, height} = document.body.getBoundingClientRect();
+    let previousEvent = 0;
 
     div2.addEventListener('click', (currentEvent) => {
-        const [horiz,vert] = [Math.floor(Math.random() * width)+1, Math.floor(Math.random() * height)+1];
+        const [horiz, vert] = [
+            Math.floor(Math.random() * width) + 1,
+            Math.floor(Math.random() * height) + 1];
+
         previousEvent++;
-        div.innerHTML = previousEvent;
+
+        div.innerHTML = String(previousEvent);
         div2.style.left = `${horiz}px`;
         div2.style.top = `${vert}px`;
     })
 };
-
-
