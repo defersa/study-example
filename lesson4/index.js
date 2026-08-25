9c462b9cdc71f069956745947e110953d2d6dbae
aleksandrM <a.makarov@zinit.com>
2026-08-25 16:28:28 +0300

    Add new endpoints

--- lesson4/index.js
+++ lesson4/index.js
@@ -8,8 +8,28 @@ app.get('/', (req, res) => {
     res.send('Привет, мир!');
 });
 
+app.post('/', (req, res) => {
+    res.send('Привет, мир!');
+});
+
+app.delete('/', (req, res) => {
+    res.send('Привет, мир!', {});
+});
+
+
 app.get('/test/', (req, res) => {
     res.send('Uh ti!');
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params.id);
+
+    res.send('ur id ' + req.params.id);
+});
+
+app.get((req, res) => {
+    res.status(404)
+        .send('Страница не найдена');
 });
 
 app.listen(port, host, (error) => {
