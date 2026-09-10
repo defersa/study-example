8f5bf8f4b14085b12860081f20ff16adc8cf2e4f
aleksandrM <a.makarov@zinit.com>
2026-09-10 15:45:58 +0300

    Start to learn react

--- app/raketa/apps/front/src/app/app.tsx
+++ app/raketa/apps/front/src/app/app.tsx
@@ -3,50 +3,77 @@
 import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
+import { EbatComponent } from './ebat';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="@org/front" />
+    const item = <div>OLOLA</div>;
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
+    const str = 'DASGFOKJLVDSHNJKLHSDFNJGHDS';
+
+    const burgers = [
+        { name: 'Ololo', price: 300 },
+        { name: 'PRICOL', price: 400 },
+    ];
+
+    const items = burgers.map((item) =>
+        <div>
+            <br/>
+            <div>{item.name}</div>
+            <div>{item.price}</div>
+        </div>
+    );
+
+    return (
+        <div>
+            {item}
+            {str}
+
+            <EbatComponent />
+
+            {items}
+
+            <NxWelcome title="@org/front" />
+
+            {/* START: routes */}
+            {/* These routes and navigation have been generated for you */}
+            {/* Feel free to move and update them to fit your needs */}
+            <br />
+            <hr />
+            <br />
+            <div role="navigation">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/page-2">Page 2</Link>
+                    </li>
+                </ul>
             </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </div>
-  );
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div>
+                            This is the generated root route.{' '}
+                            <Link to="/page-2">Click here for page 2.</Link>
+                        </div>
+                    }
+                />
+                <Route
+                    path="/page-2"
+                    element={
+                        <div>
+                            <Link to="/">
+                                Click here to go back to root page.
+                            </Link>
+                        </div>
+                    }
+                />
+            </Routes>
+            {/* END: routes */}
+        </div>
+    );
 }
 
 export default App;
---
+++ app/raketa/apps/front/src/app/ebat.tsx
@@ -0,0 +1,7 @@
+export function EbatComponent() {
+    function ebat() {
+        alert('нихуя как круто');
+    }
+
+    return <button onClick={ebat}>ДАВАЙ ЕБАТ</button>;
+}
